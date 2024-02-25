146c89913314efeb30d1d61060951f21345ad30b
Rameez Ahmad <rameezahmad20131a4245@gmail.com>
2024-02-25 19:11:07 +0530

    Changed in Hello world

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
+import React from "react";
 
-export default function Home() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
-            <h1 className="text-white">Hello World</h1>
-        </main>
-    );
-}
+const Home = () => {
+    return <div>Home Page</div>;
+};
+
+export default Home;
