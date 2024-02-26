f16610a6e60d95358e16ddd486f3947000092d79
Rameez Ahmad <rameezahmad20131a4245@gmail.com>
2024-02-26 21:38:43 +0530

    Made few changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Link from "next/link";
 
 const Home = () => {
-    return <div>Home Page</div>;
+    return(
+        <div>
+            <h1>Home Page</h1>
+            <ul>
+                <li><Link href="/about">About</Link></li>
+                <li><Link href="/blog">Blog</Link></li>
+                <li><Link href="/products">Products</Link></li>
+            </ul>
+        </div>
+    );
 };
 
 export default Home;
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,13 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
 const ProductPage = () => {
+
+    const prod_id = 123;
+
     return (
         <div className="products">
             <h1>Products List</h1>
             <ul>
-                <li>Product-1</li>
-                <li>Product-2</li>
-                <li>Product-3</li>
+                <li><Link href={`/products/1`}>Product- 1</Link></li>
+                <li><Link href={`/products/2`}>Product- 2</Link></li>
+                {/* repalce prop will replace current history state and takes to home page on back button click */}
+                <li><Link href={`/products/3`} replace>Product- 3</Link></li>
+                <li><Link href={`/products/${prod_id}`}>Product- {prod_id}</Link></li>
             </ul>
         </div>
     )
