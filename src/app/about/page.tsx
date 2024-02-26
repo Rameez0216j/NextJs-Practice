e5d8fceccc5a91931ad6305cc7960c45eedcd9c6
Rameez Ahmad <rameezahmad20131a4245@gmail.com>
2024-02-26 20:17:58 +0530

    Made few changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+export const metadata ={
+    title:"About Title",
+    description:"About page description"
+}
 
 const AboutPage = () => {
     return <div>AboutPage</div>;
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,10 +1,46 @@
 import React from "react";
 
-const SingleProductPage = ({ params }: {
+// Dynamic Metadata
+type Props = {
     params: {
         productId: string
     }
-}) => {
+}
+
+import { Metadata } from "next";
+
+// function name must be "generateMetadata"
+// export const generateMetadata= ({params} : Props) : Metadata => {
+//     return {
+//         title: `Product ${params.productId}`,
+//         description: `Product ${params.productId} details`
+//     }
+// }
+
+// We can also use async and await ( api calls and using metadata from that )
+export const generateMetadata= async  ({params} : Props) : Promise<Metadata> => {
+
+    const title : string = await new Promise((resolve, reject) => {
+        try{
+            setTimeout(()=>{
+                resolve(`Dynamic async await Product ${params.productId}`)
+            },2000)
+        }catch(error){
+            reject("No data found")
+        }
+    
+    })
+    return {
+        title: `Product ${title}`,
+        description: `Product ${params.productId} details`
+    }
+}
+
+
+// Dynamic Metadata end 
+
+
+const SingleProductPage = ({ params }: Props) => {
     return (
         <div>
             <h1>Single product details</h1>
