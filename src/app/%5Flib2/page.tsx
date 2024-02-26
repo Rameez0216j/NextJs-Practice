f2968f102679f5c20ad3048bedbd44f5c163681a
Rameez Ahmad <rameezahmad20131a4245@gmail.com>
2024-02-26 17:04:12 +0530

    Made few changes

---
+++ src/app/%5Flib2/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+// This will be included in routing 
+const NotPrivatePage = () => {
+    return <div>NotPrivatePage</div>;
+};
+
+export default NotPrivatePage;
+
---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+// This will not be included in routing
+const PrivatePage = () => {
+  return <div>PrivatePage</div>;
+};
+
+export default PrivatePage;
+
