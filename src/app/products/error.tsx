759c1ebd34cee9942cdaee52c94b48fba5ae4f18
Rameez Ahmad <rameezahmad20131a4245@gmail.com>
2024-02-27 14:18:17 +0530

    Made some changes

--- src/app/products/error.tsx
+++ src/app/products/error.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 // wrap up all nested childrens
+// To handle errors in layput use the error.tsx in parent folder of layput folder(i.e current folder's parent folder)
 export default function ErrorBoundary({ error }: {
     error: Error
 }) {
